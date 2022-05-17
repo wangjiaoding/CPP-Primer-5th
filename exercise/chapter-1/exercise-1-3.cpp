98d5b1ad64395012df6a5b3b1505c33033b7ea90
王教鼎 <onejiaoding@gmail.com>
2022-05-17 21:20:19 +0800

    add exercise 1.3,1.4

---
+++ exercise/chapter-1/exercise-1-3.cpp
@@ -0,0 +1,11 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2022-05-17 21:06:29
+ * @Description: 练习1.3
+ */
+#include <iostream>
+int main()
+{
+    std::cout << "Hello, World.\n";
+    return 0;
+}
---
+++ exercise/chapter-1/exercise-1-4.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2022-05-17 21:09:04
+ * @Description: 练习1.4
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+    return 0;
+}
