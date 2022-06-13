32ca4f1c170665f0b893353594606fdda5f2c1a0
王教鼎 <onejiaoding@gmail.com>
2022-06-13 21:49:56 +0800

    add

---
+++ example/chapter-1/example-1-4.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2022-06-13 20:48:03
+ * @Description: 例子1.4
+ */
+#include <iostream>
+
+int main()
+{
+    int sum = 0, val = 1;
+    // 只要val的值小于等于10，while循环就会持续执行
+    while (val <= 10)
+    {
+        sum += val; // 将sum + val赋予sum
+        ++val; // 将于val + 1
+    }
+    std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+    return 0;
+}
